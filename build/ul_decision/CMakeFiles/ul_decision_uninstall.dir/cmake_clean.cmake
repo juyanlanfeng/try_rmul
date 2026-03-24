2302286d413dc5867b4ed8ec47eeacd5486ce3bf
juyanlanfeng <3557677639@qq.com>
2026-03-24 13:14:57 +0800

    适配wsl

--- build/ul_decision/CMakeFiles/ul_decision_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ul_decision_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ul_decision_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
